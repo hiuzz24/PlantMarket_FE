b4b3aa38a273b9a3ac42c97fbc0538e4cff5c31a
hiuzz24 <hieupthe181180@fpt.edu.vn>
2026-01-27 02:08:50 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { RouterProvider } from 'react-router-dom';
-import router from './routers/router.jsx';
+import router from './routers/Router.jsx';
 import { ToastContainer } from 'react-toastify';
 
 createRoot(document.getElementById('root')).render(
